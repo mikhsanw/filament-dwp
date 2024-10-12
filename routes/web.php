1b3990c8edff02adb97224523698efc7adf19fbc
Muhammad Ikhsan Wibowo <ikhsan.flip@gmail.com>
2024-10-12 08:44:35 +0700

    redirect

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    //redirect to
-    return redirect()->route('admin.login');
+    //redirect to url
+    return redirect()->to('admin/login');
 });
